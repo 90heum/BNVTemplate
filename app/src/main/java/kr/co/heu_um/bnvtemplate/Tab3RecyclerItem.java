756f09819d8d02f563f38fc5dfe0317ce5fdfd69
90heum <792wjdgma@naver.com>
2021-02-05 16:17:52 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/heu_um/bnvtemplate/Tab3RecyclerItem.java
@@ -0,0 +1,20 @@
+package kr.co.heu_um.bnvtemplate;
+
+public class Tab3RecyclerItem {
+
+    String imgUrl;
+    String title;
+    String msg;
+    String rating;
+
+    public Tab3RecyclerItem() {
+
+    }
+
+    public Tab3RecyclerItem(String imgUrl, String title, String msg, String rating) {
+        this.imgUrl = imgUrl;
+        this.title = title;
+        this.msg = msg;
+        this.rating = rating;
+    }
+}
